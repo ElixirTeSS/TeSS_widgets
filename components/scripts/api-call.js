8e2bbc9bc310aed23e8738b4b18eb05540c81648
Martin Cook <martin.cook@elixir-europe.org>
2017-10-11 08:17:49 +0100

    Clean up eventsJsonGet method call - pull out query parameneters and callback function.

--- components/scripts/api-call.js
+++ components/scripts/api-call.js
@@ -2,67 +2,66 @@
 // in local scope.
 (function () {
 
-'use strict';
+	'use strict';
 
-// Variables
-var app = require("biojs-rest-tessapi");
-var api = new app.EventsApi(); // Allocate the API class we're going to use.
-var resultsDiv = document.getElementById('results');
-var startDate;
-var eventDayOfMonth;
-var eventMonth;
-var eventYear;
-var displayDate;
-var html;
-var dayName = new Array("Sunday","Monday","Tuesday","Wednesday", "Thursday",
-  "Friday","Saturday");
-var month = new Array("January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December");
+	// Variables
+	var app = require("biojs-rest-tessapi");
+	var api = new app.EventsApi();
+	var resultsDiv = document.getElementById('results');
+	var startDate;
+	var eventDayOfMonth;
+	var eventMonth;
+	var eventYear;
+	var displayDate;
+	var html;
+	var dayName = new Array("Sunday","Monday","Tuesday","Wednesday", "Thursday",
+	  "Friday","Saturday");
+	var month = new Array("January", "February", "March", "April", "May", "June",
+	  "July", "August", "September", "October", "November", "December");
 
-// Construct the query, process returned data, print the HTML
-//
-// Notes:
-// 1. Split these three actions up.
-// 2. Pull the raw parameters and callback out of eventsJsonGet() here and pass
-// them in instead? So:
-// api.eventsJsonGet(queryParameters, callbackFunction);
-// 3. Put query parameters into a 'settings' file?
-// 4. Make callbackFunction create variables that are printed into a separate template?
-api.eventsJsonGet (
-	{
-    "q": "RNA-SEQ",
-		"country[]": ["United Kingdom"]
-	}, //see lib/api/EventsApi.js for full params options
-	function(error, data, response){
-    html = '<h1>Events</h1>';
-    html += '<table><tr><th>Date</th><th>Name</th><th>Location</th></tr>';
-    data.forEach(function(value){
-      // Refactor date conversion into a function/method
-      // or use moment.js
-      startDate = new Date(value['start']);
-      eventDayOfMonth = startDate.getDay();
-      eventMonth = month[startDate.getMonth()];
-      eventYear = startDate.getFullYear();
-      displayDate = eventDayOfMonth + ' ' + eventMonth + ' ' + eventYear;
-      //eventDay = dayName[startDate.getDay()];
+	// Capture the query parameters
+	// See lib/api/EventsApi.js for full params options.
+	// TO-DO: Put default query parameters into a 'settings' file?
+	var queryParameters = {
+		"q": "Python",
+		"country[]": ["Belgium"]
+	}
+
+	// Process returned data, print the HTML (callback function)
+	// TO-DO: Create variables that are printed into a separate template?
+	// i.e. pull the HTML out of here.
+	function processReturnedData (error, data, response){
+		html = '<h1>Events</h1>';
+		html += '<table><tr><th>Date</th><th>Name</th><th>Location</th></tr>';
+		data.forEach(function(value){
+			// Refactor date conversion into a function/method
+			// or use moment.js
+			startDate = new Date(value['start']);
+			eventDayOfMonth = startDate.getDay();
+			eventMonth = month[startDate.getMonth()];
+			eventYear = startDate.getFullYear();
+			displayDate = eventDayOfMonth + ' ' + eventMonth + ' ' + eventYear;
+			//eventDay = dayName[startDate.getDay()];
 			html += '<tr><td>' + displayDate + '</td>';
-      html += '<td><a href="' + value['url'] + '">';
-      html += value['title'] + '</a></td>';
-      if ((value['city'] !== 'null') && (value['country'] !== 'null')) {
-        value['city'] = value['city'] + ', ';
-      }
-      if(value['city'] === 'null') {
-        value['city'] = '';
-      }
-      if(value['country'] === 'null' ) {
-        value['country'] = '';
-      }
-      html += '<td>' + value['city'] + value['country'] + '</td></tr>';
+			html += '<td><a href="' + value['url'] + '">';
+			html += value['title'] + '</a></td>';
+			if ((value['city'] !== 'null') && (value['country'] !== 'null')) {
+				value['city'] = value['city'] + ', ';
+			}
+			if(value['city'] === 'null') {
+				value['city'] = '';
+			}
+			if(value['country'] === 'null' ) {
+				value['country'] = '';
+			}
+			html += '<td>' + value['city'] + value['country'] + '</td></tr>';
 		})
-    html += '</table>';
-    html += '<p><a href="' + response.req.url + '">View your results on TeSS</a></p>';
+		html += '</table>';
+		html += '<p><a href="' + response.req.url + '">View your results on TeSS</a></p>';
 		resultsDiv.innerHTML = html;
 	}
-) // End api.eventsJsonGet()
+
+	// Pass the query parameters and callback function to .eventsJsonGet
+	api.eventsJsonGet (queryParameters, processReturnedData);
 
 }()); // End anonymous function
