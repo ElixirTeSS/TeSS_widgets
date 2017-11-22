8d6a4d3dc27caa8cbc6da72436983a9c2f99f02d
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-11-22 18:08:46 +0000

    Render everything using DOM API

--- components/scripts/api-call.js
+++ components/scripts/api-call.js
@@ -1,29 +1,38 @@
 // At least temporarily, wrap in self-invoking function to keep variables
 // in local scope.
 (function () {
-
     'use strict';
 
     // Variables
     var tessApi = require('tess_json_api');
     var api = new tessApi.DefaultApi();
+    var locale = getLocale();
+    var dateFormat = { year: 'numeric', month: 'long', day: 'numeric' };
+
+    // Elements
     var container = document.getElementById('tess-list-widget');
+
     var facetsContainer = document.createElement('div');
     facetsContainer.className = 'tess-facets';
+
     var wrapper = document.createElement('div');
     wrapper.className = 'tess-wrapper';
-    wrapper.innerHTML = '<h1>Events</h1>';
+
+    var widgetTitle = document.createElement('h1');
+    widgetTitle.appendChild(document.createTextNode('Events'));
+
     var resultsContainer = document.createElement('div');
     resultsContainer.className = 'tess-results';
+
     var activeFacetsContainer = document.createElement('div');
     activeFacetsContainer.className = 'tess-active-facets';
-    resultsContainer.appendChild(activeFacetsContainer);
+
+    wrapper.appendChild(widgetTitle);
+    wrapper.appendChild(activeFacetsContainer);
     wrapper.appendChild(resultsContainer);
     container.appendChild(facetsContainer);
     container.appendChild(wrapper);
 
-    var locale = getLocale();
-    var dateFormat = { year: 'numeric', month: 'long', day: 'numeric' };
 
     // Capture the query parameters
     // See lib/api/EventsApi.js for full params options.
@@ -62,31 +71,92 @@
         });
     }
 
+    function renderEvent(container, event) {
+        var eventRow = container.insertRow();
+
+        // Date
+        var dateCell = eventRow.insertCell();
+        dateCell.appendChild(document.createTextNode(formatDate(event.attributes['start'])));
+
+        // Name
+        var nameCell = eventRow.insertCell();
+        var link = document.createElement('a');
+        link.href = event.links['redirect'];
+        link.appendChild(document.createTextNode(event.attributes['title']));
+        nameCell.appendChild(link);
+
+        // Location
+        var locCell = eventRow.insertCell();
+        var city = event.attributes['city'];
+        var country = event.attributes['country'];
+
+        if ((event.attributes['city'] !== 'null') && (event.attributes['country'] !== 'null')) {
+            city = event.attributes['city'] + ', ';
+        }
+        if (event.attributes['city'] === 'null') {
+            city = '';
+        }
+        if (event.attributes['country'] === 'null' ) {
+            country = '';
+        }
+        locCell.appendChild(document.createTextNode(city + country));
+    }
+
+    function renderEvents(container, events) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        var table = document.createElement('table');
+        var head = document.createElement('thead');
+        var body = document.createElement('tbody');
+        table.appendChild(head);
+        table.appendChild(body);
+        container.appendChild(table);
+
+        // Headings
+        var headingRow = head.insertRow();
+        ['Date', 'Name', 'Location'].forEach(function (heading) {
+           var cell = document.createElement('th');
+           cell.appendChild(document.createTextNode(heading));
+           headingRow.appendChild(cell);
+        });
+
+        // Events
+        events.forEach(function (event) {
+            renderEvent(body, event);
+        });
+    }
+
     function renderFacetRow(container, active, value, count) {
         var row = document.createElement('li');
         row.className = 'tess-facet-row' + (active ? ' active' : '');
-        row.innerHTML = value + (active ? '' : ' (' + count + ')');
+        row.appendChild(document.createTextNode(value + (active ? '' : ' (' + count + ')')));
         row.setAttribute('data-tess-facet-value', value);
         row.setAttribute('data-tess-facet-active', active);
 
         container.appendChild(row);
     }
 
-    function renderFacet(container, key, available, active) {
+    function renderFacet(container, key, availableFacets, activeFacets) {
         var category = document.createElement('div');
         category.className = 'tess-facet';
-        category.innerHTML = '<h3>' + humanize(key) + '</h3>';
         category.setAttribute('data-tess-facet-key', key);
+
+        var title = document.createElement('h3');
+        title.appendChild(document.createTextNode(humanize(key)));
+        category.appendChild(title);
+
         var list = document.createElement('ul');
         category.appendChild(list);
 
         // Render the active facets first so they appear at the top.
-        active.forEach(function (val) {
+        activeFacets.forEach(function (val) {
             renderFacetRow(list, true, val);
         });
-        available.forEach(function (row) {
+        availableFacets.forEach(function (row) {
             // Don't render active facets twice!
-            if (!active.includes(row.value)) {
+            if (!activeFacets.includes(row.value)) {
                 renderFacetRow(list, false, row.value, row.count);
             }
         });
@@ -97,6 +167,41 @@
         });
 
         container.appendChild(category);
+    }
+
+    function renderActiveFacets(container, activeFacets, searchQuery) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        var generateActiveFacet = function (key, value) {
+            var af = document.createElement('div');
+            af.className = 'tess-active-facet';
+            var afKey = document.createElement('strong');
+            afKey.appendChild(document.createTextNode(humanize(key) + ': '));
+
+            af.appendChild(afKey);
+
+            var values = Array.isArray(value) ? value : [value];
+            values.forEach(function (value, index) {
+                var afVal = document.createElement('span');
+                afVal.className = 'tess-active-facet-value';
+                afVal.appendChild(document.createTextNode(value));
+                af.appendChild(afVal);
+                if (index < (values.length - 1))
+                    af.appendChild(document.createTextNode(' or '));
+            });
+
+            return af;
+        };
+
+        if (searchQuery && searchQuery !== '') {
+            container.appendChild(generateActiveFacet('Search Query', searchQuery));
+        }
+
+        for (var key in activeFacets) {
+            container.appendChild(generateActiveFacet(key, activeFacets[key]));
+        }
     }
 
     function applyFacet(key, value) {
@@ -135,54 +240,30 @@
     // Process returned data, print the HTML (callback function)
     // TO-DO: Create variables that are printed into a separate template?
     // i.e. pull the HTML out of here.
-    function processReturnedData(error, data, response){
-        facetsContainer.innerHTML = '';
-
+    function processReturnedData(error, data, response) {
+        // Render facet sidebar
+        while (facetsContainer.firstChild) {
+            facetsContainer.removeChild(facetsContainer.firstChild);
+        }
         for (var key in data.meta['available-facets']) {
             if (data.meta['available-facets'][key].length) {
                 renderFacet(facetsContainer, key, data.meta['available-facets'][key], (data.meta['facets'][key] || []));
             }
         }
 
-        var html = '';
-        if (data.meta['query'] && data.meta['query'] !== '') {
-            html += '<strong>Search terms:</strong> "' + data.meta['query'] + '"<br/>';
-        }
+        // Render active facet bar
+        renderActiveFacets(activeFacetsContainer, data.meta['facets'], data.meta['query']);
 
-        for (var key in data.meta['facets']) {
-            html += '<strong>' + humanize(key) + ':</strong> ';
-            (data.meta['facets'][key] || []).forEach(function (activeFacet, index) {
-                html += '"' + activeFacet + '"';
-                if (index !== (data.meta['facets'][key].length - 1))
-                    html += ' or ';
-            });
-            html += '</br>';
-        }
+        // Render results
+        renderEvents(resultsContainer, data.data);
 
-        activeFacetsContainer.innerHTML = html;
-
-        html = '';
-        html += '<table><tr><th>Date</th><th>Name</th><th>Location</th></tr>';
-        data.data.forEach(function(event){
-            var attributes = event.attributes;
-            html += '<tr><td>' + formatDate(attributes['start']) + '</td>';
-            html += '<td><a href="' + event.links.redirect + '">';
-            html += attributes['title'] + '</a></td>';
-            if ((attributes['city'] !== 'null') && (attributes['country'] !== 'null')) {
-                attributes['city'] = attributes['city'] + ', ';
-            }
-            if(attributes['city'] === 'null') {
-                attributes['city'] = '';
-            }
-            if(attributes['country'] === 'null' ) {
-                attributes['country'] = '';
-            }
-            html += '<td>' + attributes['city'] + attributes['country'] + '</td></tr>';
-        });
-        html += '</table>';
-        html += '<p><a href="' + response.req.url + '">View your results on TeSS</a></p>';
-        html += '</div>';
-        resultsContainer.innerHTML = html;
+        // Render TeSS link
+        var tessLinkContainer = document.createElement('p');
+        var tessLink = document.createElement('a');
+        tessLink.href = response.req.url;
+        tessLink.appendChild(document.createTextNode('View your results on TeSS'));
+        tessLinkContainer.appendChild(tessLink);
+        resultsContainer.appendChild(tessLinkContainer);
     }
 
     function getEvents(queryParameters) {
