375951045f6727224fe7a0c98ca2856c130160b6
Finn Bacall <finn.bacall@manchester.ac.uk>
2023-01-09 13:37:52 +0000

    Simpler way of showing/hiding filters

--- components/js/renderers/faceted-table-renderer.js
+++ components/js/renderers/faceted-table-renderer.js
@@ -33,7 +33,21 @@ class FacetedTableRenderer extends Renderer {
         // Top search bar and active facets display
         this.elements.search = n('div', { className: 'tess-search' });
         this.elements.activeFacets = n('div', { className: 'tess-active-facets' });
+        // Button to show/hide facets on mobile
+        this.elements.facetToggle = n('button', { className: 'tess-facet-btn'}, 'Show Filters');
+        this.elements.facetToggle.addEventListener('click', (event) => {
+            event.preventDefault();
+            this.elements.facets.classList.toggle('tess-facets-open');
+            if (this.elements.facets.classList.contains('tess-facets-open')) {
+                this.elements.facetToggle.innerText = 'Hide Filters';
+            } else {
+                this.elements.facetToggle.innerText = 'Show Filters';
+            }
+
+            return false;
+        });
         this.elements.controls = n('div', { className: 'tess-controls' },
+            this.elements.facetToggle,
             this.elements.activeFacets,
             this.elements.search
         );
@@ -42,9 +56,7 @@ class FacetedTableRenderer extends Renderer {
         this.elements.results = n('div', { className: 'tess-results' });
         this.elements.pagination = n('div', { className: 'tess-pagination' });
         this.elements.tessLink = n('div');
-        this.elements.filterControl = n('div', { className: 'tess-filter'});
         this.elements.wrapper = n('div', { className: 'tess-wrapper' },
-            this.elements.filterControl,
             this.elements.controls,
             this.elements.results,
             this.elements.pagination,
@@ -75,31 +87,9 @@ class FacetedTableRenderer extends Renderer {
         this.elements.tessLink.appendChild(n('p',
             n('a', { href: response.req.url, target: '_blank' },
                 'View your results on TeSS')));
-
-        // TeSS filter button
-        Renderer.clear(this.elements.filterControl);
-        this.elements.filterControl.appendChild(n('button',{ className: 'btn filterButton'},n('i',{className: 'fa fa-filter'}),'Show Filter'));
-        this.elements.filterControl.appendChild(n('button',{ className: 'btn closeButton'}, n('i',{className:'fa fa-close'}),'Close Filter'));
-        var allFilterButtons = document.getElementsByClassName('filterButton');
-        var num;
-        for (var i = 0; i < allFilterButtons.length; i++) {
-            allFilterButtons[i].addEventListener('click',function(index){
-                document.getElementsByClassName('tess-facets')[index].style.display = 'block';
-                document.getElementsByClassName('tess-facets')[index].style.width = '110px';
-                document.getElementsByClassName('closeButton')[index].style.display = 'inline';
-                document.getElementsByClassName('filterButton')[index].style.display = 'none';
-            }.bind(this, i));
-        }
-
-        var allCloseButtons = document.getElementsByClassName('closeButton');
-        for (var i = 0; i < allCloseButtons.length; i++) {
-            allCloseButtons[i].addEventListener('click',function(index){
-                document.getElementsByClassName('filterButton')[index].style.display = 'block';
-                document.getElementsByClassName('tess-facets')[index].style.display = 'none';
-                document.getElementsByClassName('closeButton')[index].style.display = 'none';
-            }.bind(this, i));
-        }
     }
+
+
 
 }
 
